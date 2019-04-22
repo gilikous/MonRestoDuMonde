66804f7072b5748816918aa1ebce36d6656d1b4e
Gilikous <richard.bryan1701@gmail.com>
2019-04-22 15:53:57 +0200

    Ajout des tabbar

---
+++ MonRestoDuMonde/MonRestoDuMonde/Controller/MonTabBar.swift
@@ -0,0 +1,24 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by Bryan Richard on 22/04/2019.
+//  Copyright © 2019 Bryan Richard. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+        
+        
+    }
+    
+
+
+}
---
+++ MonRestoDuMonde/MonRestoDuMonde/Utilitaires/Constantes.swift
@@ -0,0 +1,15 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by Bryan Richard on 22/04/2019.
+//  Copyright © 2019 Bryan Richard. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(displayP3Red: 30/255, green: 30/255, blue: 30/255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(displayP3Red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+
